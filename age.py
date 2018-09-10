38ae70e8671141265a76f5657e29943f91716d15
peace <astonreba@gmail.com>
2018-09-10 18:28:27 +0300

    resolved some errors in age.py

--- age.py
+++ age.py
@@ -1,19 +1,25 @@
 #importing the datetime module
 from datetime import datetime
+
 #defining the function that will calculate the age
-def calculate_age(yob):
+def calculate_age(age):
 
-    current=datetime.now().year
-    age =current-int(yob)
     if (age<0):
-        y="Your age cannot be negative"
+        return "Your age cannot be negative"
     elif(age<18):
-        y="You are a minor"
+        return "You are a minor"
     elif(18<age<36):
-        y="You are a youth"
+        return "You are a youth"
     else:
-        y="You are an elder"
-    return(y)    
+        return "You are an elder"
+    
 
+current=datetime.now().year
 yob = int(input("Enter year of birth:"))
-print (calculate_age(yob))
+age = current-int(yob)
+print (calculate_age(age))
+    
+
+   
+     
+    
