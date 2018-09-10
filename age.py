c02f810792ca7c3293f193ef87a325c671f9cfeb
peace <astonreba@gmail.com>
2018-09-10 22:17:24 +0300

    edited age.py

--- age.py
+++ age.py
@@ -5,11 +5,12 @@ def calculate_age(yob):
 
     current=datetime.now().year
     age =current-int(yob)
+    
     if (age<0):
         y="Your age cannot be negative"
     elif(age<18):
         y="You are a minor"
-    elif(18<age<36):
+    elif(18<=age<=36):
         y="You are a youth"
     else:
         y="You are an elder"
