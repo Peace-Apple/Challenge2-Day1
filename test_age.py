c88dc495e79433973bf5865fc435be64c1387a43
peace <astonreba@gmail.com>
2018-09-11 22:02:38 +0300

    edited the test file test_age.py

--- test_age.py
+++ test_age.py
@@ -3,10 +3,10 @@ from age import calculate_age
 
 class Test(TestCase):
     def test_age(self):
-        self.assertEquals(calculate_age(18),"You are a minor", "You are a youth")
-        self.assertEquals(calculate_age(-1),"You are a minor", "Your age cannot be negative")
-        self.assertEquals(calculate_age(20),"You are an elder", "You are a youth")
-        self.assertEquals(calculate_age(10),"You are a youth", "You are a minor")
+        self.assertEquals(calculate_age(2000), "You are a youth")
+        self.assertEquals(calculate_age(2019), "Your age cannot be negative")
+        self.assertEquals(calculate_age(1995), "You are a youth")
+        self.assertEquals(calculate_age(2005),"You are a minor")
 
 
 
