ea439fd65046caa7fa8c7a450ee3bb44335485fa
peace <astonreba@gmail.com>
2018-09-11 22:34:51 +0300

    trying to run the tests

---
+++ test_age.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from age import calculate_age
+
+class Test(TestCase):
+    def testAge(self):
+        self.assertEquals(calculate_age(2000), "You are a youth")
+        self.assertEquals(calculate_age(2019), "Your age cannot be negative")
+        self.assertEquals(calculate_age(1995), "You are a youth")
+        self.assertEquals(calculate_age(2005),"You are a minor")
+
+
+
+
+        
